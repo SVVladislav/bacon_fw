e41f1e320679f7b49e085e29a16371f246ab7760
SVVladislav <vsagunovs@gmail.com>
2024-03-23 22:35:02 +0300

    Перетасовал папки, добавил Microblaze

--- HW/bacon_v9/bacon_fw.srcs/sources_1/imports/hdl/bacon_wrapper.v
+++ HW/bacon_v9/bacon_fw.srcs/sources_1/imports/hdl/bacon_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2023.2.2 (win64) Build 4126759 Thu Feb  8 23:53:51 MST 2024
-//Date        : Fri Mar 22 22:47:56 2024
+//Date        : Sat Mar 23 22:23:37 2024
 //Host        : Zen running 64-bit major release  (build 9200)
 //Command     : generate_target bacon_wrapper.bd
 //Design      : bacon_wrapper
@@ -12,14 +12,14 @@
 
 module bacon_wrapper
    (clk_in1,
-    sout);
+    clk_out_156M25);
   input clk_in1;
-  output sout;
+  output clk_out_156M25;
 
   wire clk_in1;
-  wire sout;
+  wire clk_out_156M25;
 
   bacon bacon_i
        (.clk_in1(clk_in1),
-        .sout(sout));
+        .clk_out_156M25(clk_out_156M25));
 endmodule
